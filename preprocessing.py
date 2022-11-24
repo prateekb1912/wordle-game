a234b3a8f15b17e419b35f507f28a4e4a8e82fed
prateekb1912 <prateekb1912@gmail.com>
2022-11-24 19:32:16 +0530

    limit words dataset to 5000

--- preprocessing.py
+++ preprocessing.py
@@ -4,4 +4,6 @@ words_df = pd.read_csv('words.csv')
 
 words_df.sort_values(by='occurrence', ascending=False, inplace=True)
 
-words_df.to_csv('popluar_words_1000.csv', sep=',', index=False)
+popular_words = words_df.head(5000)
+
+popular_words.to_csv('popluar_words_5000.csv', sep=',', index=False, )
