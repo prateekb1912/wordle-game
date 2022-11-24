217727ce7aa4e73b631e8f47bb133bbb9b68ba1d
prateekb1912 <prateekb1912@gmail.com>
2022-11-24 22:20:26 +0530

    format letters based on guess

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ with open('popular_words_5000.txt', 'r') as file:
 WELCOME_MESSAGE = f'\n[white on blue] WELCOME TO WORDLE [/]\n'
 ALLOWED_GUESSES = 6
 
-
+CORRECT_PLACE = 'bold black on green'
+CORRECT_LETTER = 'bold white on yellow'
+INCORRECT_LETTER = 'bold white on black'
 
 if __name__ == '__main__':
     console = Console()
 
     chosen_word = choice(words)
     console.print(WELCOME_MESSAGE)
-    console.print(chosen_word)
+
+    console.print('A', style=CORRECT_PLACE)
+    console.print('B', style=CORRECT_LETTER)
+    console.print('B', style=INCORRECT_LETTER)
